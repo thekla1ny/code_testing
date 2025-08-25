d89b55ace1ee65de986be81e8c71fea5c1f6dbba
thekla1ny <96620706+thekla1ny@users.noreply.github.com>
2025-08-25 16:27:19 +0500

    Update mastercode.py

--- mastercode.py
+++ mastercode.py
@@ -3,13 +3,31 @@ import asyncio
 
 # Константы
 SOURCE_CHAT_ID = "me"
-TARGET_CHAT_ID = -1001984966725
-WHITELIST = [543436534653]  # ✅ Укажи разрешённые Telegram ID
 
-print("авторизация через телеграм. надо ввести данные. Создал @KingOfInsanity")
+# Список чатов для рассылки
+TARGET_CHAT_IDS = [
+    -1002473605725,
+    -1002361289229,
+    -1002318593167,
+    -1002311406040,
+    -1002329838424,
+    -1001984966725,
+    -1001996734240,
+    -1002391093375,
+    -1002491340826,
+    -1002037962968,
+    -1002475503071,
+    -1002327591798,
+    -1002395514384
+]
+
+WHITELIST = [7412272825, 5436806218, 7579684665]  
+
+print("авторизация через телеграм. надо ввести данные. Создал @KingOfInsanity. версия 2")
 api_id = int(input("Введи свой API ID: "))
 api_hash = input("Введи свой API HASH: ")
 session_name = "akkaunt_data"
+
 
 # Проверка ID пользователя
 async def get_user_id():
@@ -18,6 +36,7 @@ async def get_user_id():
         print(f"👤 Ваш Telegram ID: {user.id}")
         return user.id
 
+
 # Основная логика пересылки
 async def run_forwarder():
     async with Client(session_name, api_id=api_id, api_hash=api_hash) as app:
@@ -25,18 +44,20 @@ async def run_forwarder():
         while True:
             async for message in app.get_chat_history(SOURCE_CHAT_ID, limit=1):
                 if message:
-                    try:
-                        await app.forward_messages(
-                            chat_id=TARGET_CHAT_ID,
-                            from_chat_id=SOURCE_CHAT_ID,
-                            message_ids=message.id
-                        )
-                        print("📤 Сообщение переслано.")
-                    except Exception as e:
-                        print("❌ Ошибка пересылки:", e)
+                    for target_id in TARGET_CHAT_IDS:
+                        try:
+                            await app.forward_messages(
+                                chat_id=target_id,
+                                from_chat_id=SOURCE_CHAT_ID,
+                                message_ids=message.id
+                            )
+                            print(f"📤 Сообщение переслано в {target_id}.")
+                        except Exception as e:
+                            print(f"❌ Ошибка пересылки в {target_id}: {e}")
                 else:
                     print("ℹ️ Нет сообщений в избранном.")
-            await asyncio.sleep(3600)
+            await asyncio.sleep(3600)  # ждем 1 час
+
 
 # Главная функция
 async def main():
@@ -48,6 +69,7 @@ async def main():
             await asyncio.sleep(3600)  # Висим, не закрываемся
     else:
         await run_forwarder()
+
 
 # Запуск
 if __name__ == "__main__":
